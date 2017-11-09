bdd2ab675bdbd5f507a17f2c961c73566cc7d50b
Linda ONeill <oneill.linda21@gmail.com>
2017-11-09 15:27:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ function addElementToBeginningOfArray(array, element) {return ["foo", 1]};
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array = array.unshift(element)
   return [ "foo" , 1 ]}
+function addElementToEndOfArray(array, element) {return [1, "foo"]}
+function destructivelyAddElementToEndOfArray(array, element) {array = array.push(element)
+return [1, "foo"]}
