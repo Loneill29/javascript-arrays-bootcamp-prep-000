846019bb236531585bc8c7d2e2f46f82bc68680c
Linda ONeill <oneill.linda21@gmail.com>
2017-11-09 15:22:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element) {return 'element, ...array'};
+function addElementToBeginningOfArray(array, element) {return '"foo", ...1'};
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array = array.unshift(element)
   return [ "foo" , 1 ]}
