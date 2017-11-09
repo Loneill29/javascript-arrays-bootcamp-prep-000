332b1b394059d3ec2e1ffe82199f5583ca563aca
Linda ONeill <oneill.linda21@gmail.com>
2017-11-09 14:42:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var colors = ["blue", "red"];
 function addElementToBeginningOfArray() {return '"green", ...colors'}
+function destructivelyAddElementToBeginningOfArray() {return 'cities.unshift("green")'}
